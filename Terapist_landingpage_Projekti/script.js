3bed1a44fea8657ab7f7411a9dfc886d25fa0a76
Kohila Karunakaran <kohilakaruna@gmail.com>
2025-11-19 11:21:24 +0200

    Proh

---
+++ Terapist_landingpage_Projekti/script.js
@@ -0,0 +1,39 @@
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+function currentSlide(n) {
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[n - 1].style.display = "block";
+  dots[n - 1].className += " active";
+  slideIndex = n;
+}
+
+
+  
